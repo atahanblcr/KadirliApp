7ff0807b7a69d9e0a4e18eea09eec898f2358a13
Atahan <atahanblcr@icloud.com>
2025-12-08 00:52:11 +0300

    Initial Commit

---
+++ KadirliApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KadirliApp
+//
+//  Created by MUSTAFA ATAHAN on 8.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KadirliApp/KadirliAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KadirliAppApp.swift
+//  KadirliApp
+//
+//  Created by MUSTAFA ATAHAN on 8.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct KadirliAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
